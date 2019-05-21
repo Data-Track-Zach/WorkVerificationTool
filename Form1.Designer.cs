40fd62b8b606f83eec857babfe681addaf958d88
zachcar <30635776+zachcar@users.noreply.github.com>
2019-05-21 07:25:46 -0700

    C# Source Code

---
+++ Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace ImageVerificationTool
+{
+    partial class IVT
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CheckedListFlags = new System.Windows.Forms.CheckedListBox();
+            this.OutputText = new System.Windows.Forms.TextBox();
+            this.Btn_Store = new System.Windows.Forms.Button();
+            this.ImageLocationComboBox = new System.Windows.Forms.ComboBox();
+            this.BTN_Clear = new System.Windows.Forms.Button();
+            this.Btn_Copy = new System.Windows.Forms.Button();
+            this.NameTextBox = new System.Windows.Forms.TextBox();
+            this.DateTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // CheckedListFlags
+            // 
+            this.CheckedListFlags.CheckOnClick = true;
+            this.CheckedListFlags.ColumnWidth = 600;
+            this.CheckedListFlags.Enabled = false;
+            this.CheckedListFlags.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CheckedListFlags.FormattingEnabled = true;
+            this.CheckedListFlags.Location = new System.Drawing.Point(237, 8);
+            this.CheckedListFlags.Name = "CheckedListFlags";
+            this.CheckedListFlags.Size = new System.Drawing.Size(397, 340);
+            this.CheckedListFlags.TabIndex = 0;
+            this.CheckedListFlags.ItemCheck += new System.Windows.Forms.ItemCheckEventHandler(this.CheckedListFlags_ItemCheck);
+            // 
+            // OutputText
+            // 
+            this.OutputText.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.OutputText.Location = new System.Drawing.Point(739, 9);
+            this.OutputText.Multiline = true;
+            this.OutputText.Name = "OutputText";
+            this.OutputText.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.OutputText.Size = new System.Drawing.Size(380, 339);
+            this.OutputText.TabIndex = 1;
+            this.OutputText.TextChanged += new System.EventHandler(this.OutputText_TextChanged);
+            // 
+            // Btn_Store
+            // 
+            this.Btn_Store.Enabled = false;
+            this.Btn_Store.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Btn_Store.Location = new System.Drawing.Point(640, 8);
+            this.Btn_Store.Name = "Btn_Store";
+            this.Btn_Store.Size = new System.Drawing.Size(93, 26);
+            this.Btn_Store.TabIndex = 2;
+            this.Btn_Store.Text = "Store";
+            this.Btn_Store.UseVisualStyleBackColor = true;
+            this.Btn_Store.Click += new System.EventHandler(this.Btn_Store_Click);
+            // 
+            // ImageLocationComboBox
+            // 
+            this.ImageLocationComboBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ImageLocationComboBox.FormattingEnabled = true;
+            this.ImageLocationComboBox.Location = new System.Drawing.Point(12, 40);
+            this.ImageLocationComboBox.Name = "ImageLocationComboBox";
+            this.ImageLocationComboBox.Size = new System.Drawing.Size(219, 28);
+            this.ImageLocationComboBox.TabIndex = 3;
+            this.ImageLocationComboBox.SelectedIndexChanged += new System.EventHandler(this.ImageLocationComboBox_SelectedIndexChanged);
+            // 
+            // BTN_Clear
+            // 
+            this.BTN_Clear.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BTN_Clear.Location = new System.Drawing.Point(640, 321);
+            this.BTN_Clear.Name = "BTN_Clear";
+            this.BTN_Clear.Size = new System.Drawing.Size(93, 27);
+            this.BTN_Clear.TabIndex = 5;
+            this.BTN_Clear.Text = "Clear All";
+            this.BTN_Clear.UseVisualStyleBackColor = true;
+            this.BTN_Clear.Click += new System.EventHandler(this.BTN_Clear_Click);
+            // 
+            // Btn_Copy
+            // 
+            this.Btn_Copy.Enabled = false;
+            this.Btn_Copy.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Btn_Copy.Location = new System.Drawing.Point(640, 95);
+            this.Btn_Copy.Name = "Btn_Copy";
+            this.Btn_Copy.Size = new System.Drawing.Size(93, 28);
+            this.Btn_Copy.TabIndex = 8;
+            this.Btn_Copy.Text = "Copy";
+            this.Btn_Copy.UseVisualStyleBackColor = true;
+            this.Btn_Copy.Click += new System.EventHandler(this.BtnCopy_Click);
+            // 
+            // NameTextBox
+            // 
+            this.NameTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.NameTextBox.Location = new System.Drawing.Point(12, 8);
+            this.NameTextBox.Name = "NameTextBox";
+            this.NameTextBox.Size = new System.Drawing.Size(107, 26);
+            this.NameTextBox.TabIndex = 9;
+            this.NameTextBox.Text = "Name";
+            // 
+            // DateTextBox
+            // 
+            this.DateTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DateTextBox.Location = new System.Drawing.Point(125, 8);
+            this.DateTextBox.Name = "DateTextBox";
+            this.DateTextBox.Size = new System.Drawing.Size(106, 26);
+            this.DateTextBox.TabIndex = 10;
+            this.DateTextBox.Text = "Date";
+            // 
+            // IVT
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1128, 360);
+            this.Controls.Add(this.DateTextBox);
+            this.Controls.Add(this.NameTextBox);
+            this.Controls.Add(this.Btn_Copy);
+            this.Controls.Add(this.BTN_Clear);
+            this.Controls.Add(this.ImageLocationComboBox);
+            this.Controls.Add(this.Btn_Store);
+            this.Controls.Add(this.OutputText);
+            this.Controls.Add(this.CheckedListFlags);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "IVT";
+            this.ShowIcon = false;
+            this.Text = "Image Verification Tool";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckedListBox CheckedListFlags;
+        private System.Windows.Forms.TextBox OutputText;
+        private System.Windows.Forms.Button Btn_Store;
+        private System.Windows.Forms.ComboBox ImageLocationComboBox;
+        private System.Windows.Forms.Button BTN_Clear;
+        private System.Windows.Forms.Button Btn_Copy;
+        private System.Windows.Forms.TextBox NameTextBox;
+        private System.Windows.Forms.TextBox DateTextBox;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ImageVerificationTool
+{
+    public partial class IVT : Form
+    {
+        public IVT()
+        {
+            InitializeComponent();
+            FlagStartup();
+            LocationStartup();
+
+        }
+        // Initialize Flags
+        // List Method
+        // Initialize List
+        List<string> FlagList = new List<string>
+            {
+            // Add List items (manually)
+                "Reason A",
+                "Reason B",
+                "Reason C",
+                "Reason D",
+                "Reason E",
+                "Reason F",
+                "Reason G",
+                "Reason H",
+                "Reason I",
+                "Reason J",
+                "Reason K",
+                "Reason L",
+                "Reason M",
+                "Reason N"
+            };
+        void FlagStartup()
+        {
+            // Add each item to the checkListFlags
+            foreach (var flag in FlagList)
+            {
+                CheckedListFlags.Items.Add(flag);
+            }
+        }
+
+        // Initialize ImageLocations
+        List<string> ImageLocationsList = new List<string>
+            {
+                // Add List items (manually)
+                "Location 1",
+                "Location 2",
+                "Location 3",
+                "Location 4",
+                "Location 5",
+                "Location 6",
+                "Location 7",
+                "Location 8",
+                "Location 9",
+                "Location 10",
+                "Location 11"
+            };
+        // Add each item to the checkListFlags
+        void LocationStartup()
+        {
+            foreach (var location in ImageLocationsList)
+            {
+                ImageLocationComboBox.Items.Add(location);
+            }
+        }
+        // Initialize storage containers
+        public static Dictionary<string, List<string>> ImageFlags = new Dictionary<string, List<string>>();
+        public static List<string> CheckedFlags = new List<string>();
+
+        // Clear previous output (Image Locations, related Flags, and output text)
+        void RefreshCheckedListFlags()
+        {
+            foreach (int i in CheckedListFlags.CheckedIndices)
+            {
+                CheckedListFlags.SetItemCheckState(i, CheckState.Unchecked);
+            }
+        }
+        void FullRefresh()
+        {
+            OutputText.Clear();
+            ImageLocationComboBox.Text = "";
+            RefreshCheckedListFlags();
+
+            CheckedListFlags.Enabled = false;
+            Btn_Store.Enabled = false;
+        }
+
+        // Create a list containing CheckedFlags based on the CheckedListBox
+        List<string> StoreFlags(CheckedListBox CLB, List<string> FlagList)
+        {
+            // Clear previous list contents
+            FlagList.Clear();
+            // Update the list contents from new CheckListBox checked boxes
+            // Iterate over CheckedListFlags to find the Checked Flags
+            foreach (string item in CLB.CheckedItems)
+            {
+                // Add Flag to the list
+                FlagList.Add(item);
+            }
+            // Return the list of checked flags
+            return FlagList;
+        }
+
+        private void Btn_Store_Click(object sender, EventArgs e)
+        {
+            // Update Flags
+            StoreFlags(CheckedListFlags, CheckedFlags);
+
+            // Create a temporary list
+            List<string> SpecifcFlags = new List<string>(CheckedFlags);
+
+            // Add Dictionary Key/Content Pair
+            ImageFlags.Add(ImageLocationComboBox.Text, SpecifcFlags);
+
+            // Remove Key from ImageLocationComboBox
+            ImageLocationComboBox.Items.Remove(ImageLocationComboBox.Text);
+            //Reset Functionality
+            FullRefresh();
+
+            OutputText.AppendText(NameTextBox.Text + " [" + "Image Check" + "] " + "(" + DateTextBox.Text + ")");
+            foreach (var contents in ImageFlags.Keys)
+            {
+                OutputText.AppendText("\r\n" + contents);
+                foreach (var listItem in ImageFlags[contents])
+                {
+                    OutputText.AppendText("\r\n"+"   "+"\u2022 " + listItem);
+                }
+            }
+            OutputText.AppendText("\r\n");
+
+        }
+
+
+        private void Btn_Print_Click(object sender, EventArgs e)
+        {
+
+            // Print Key and Content pair
+            OutputText.Clear();
+
+            OutputText.AppendText(NameTextBox.Text + " [" + "Image Check" + "] " + "(" + DateTextBox.Text + ")");
+            foreach (var contents in ImageFlags.Keys)
+            {
+                OutputText.AppendText("\r\n" + contents);
+                foreach (var listItem in ImageFlags[contents])
+                {
+                    OutputText.AppendText("\r\n" + "   " + "\u2022 " + listItem);
+                }
+            }
+
+        }
+
+        // Button Activation
+        private void ImageLocationComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            CheckedListFlags.Enabled = true;
+        }
+        private void CheckedListFlags_ItemCheck(object sender, ItemCheckEventArgs e)
+        {
+            Btn_Store.Enabled = true;
+        }
+
+        private void BTN_Clear_Click(object sender, EventArgs e)
+        {
+            FullRefresh();
+            ImageFlags.Clear();
+            ImageLocationComboBox.Items.Clear();
+            LocationStartup();
+        }
+
+        private void BtnCopy_Click(object sender, EventArgs e)
+        {
+
+            Clipboard.SetText(OutputText.Text);
+        }
+
+        private void OutputText_TextChanged(object sender, EventArgs e)
+        {
+            Btn_Copy.Enabled = !string.IsNullOrWhiteSpace(OutputText.Text);
+        }
+    }
+
+
+}
